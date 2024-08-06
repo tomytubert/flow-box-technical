4135e58587a6b43d5cc3f83dc6845899cad9b8a4
tomytubert <tomytubert@gmail.com>
2024-08-06 11:38:06 +0200

    Added selfhosted font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Button } from '@mui/material'
 import './App.css'
 import useFetch from './hooks/useFetch'
+
 function App() {
 const { data, error, loading } = useFetch(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=dogs&per_page=50`)
 
   return (
     <>
-      {loading ? <h1>Loading...</h1> : JSON.stringify(data)}
+      <Button>Hola mundo</Button>
     </>
   )
 }
